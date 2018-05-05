98afbc8241a9377e7b3df86ab90a8bad15a8c720
unknown <https://github.com/JhonatanRios>
2018-05-05 13:19:51 -0500

    Revición de filtro funciona la primera parte, Creacion del archivo Check

--- killme.js
+++ killme.js
@@ -1,9 +1,10 @@
-const MongoClient = require('mongodb').MongoClient,
-express = require('express'),
-engines = require('consolidate');
+const MongoClient = require('mongodb').MongoClient
+ObjectID = require('mongodb').ObjectID,
+    express = require('express'),
+    engines = require('consolidate');
 
 var app = express(),
-db;
+    db;
 
 app.engine('hbs', engines.handlebars);
 
@@ -14,67 +15,79 @@ app.use(express.static('public'));
 
 // Conectarse a Base de Datos
 MongoClient.connect('mongodb://localhost:27017', function (err, client) {
-if (err) throw err;
+    if (err) throw err;
 
-db = client.db('test');
+    db = client.db('test');
 
-// Iniciar servidor
-app.listen(1234);
-console.log("Escuchando servidor")
+    // Iniciar servidor
+    app.listen(1234);
+    console.log("Escuchando servidor")
 });
 
 /*Esta parte es para cargar las paginas*/
 app.get('/', (req, res) => {
-    res.render('home', {
-    });
+    res.render('home', {});
 })
 
 app.get('/how', (req, res) => {
-    res.render('how', {
-    });
+    res.render('how', {});
 })
 
 app.get('/book', (req, res) => {
-    res.render('books', {
-    });
+    res.render('books', {});
 })
 
 app.get('/reco', (req, res) => {
-    /*db.collection('productos')
-        .find()
-        .toArray((err, result) => {
-            res.render('index', {
-                productos: result
-            });
-        })*/
-
     var prod = db.collection('libros')
         .find();
-    
-    if(req.query.calificacion)
-        prod.filter({ calificacion: req.query.calificacion });
 
-    if(req.query.modelo)
-        prod.filter({ modelo: req.query.modelo });
+    if (req.query.editorial)
+        prod.filter({
+            editorial: req.query.editorial
+        });
+
+    if (req.query.modelo)
+        prod.filter({
+            modelo: req.query.modelo
+        });
 
     prod.toArray((err, result) => {
-            res.render('recommended', {
-                libros: result
-            });
-        })
+        res.render('recommended', {
+            libros: result,
+            tittle: "Recommended"
+        });
+    })
 });
 
-/*app.get('/contact', (req, res) => {
-    res.render('contact', {
-    });
-})*/
-
 app.get('/contact', (req, res) => {
-    res.render('contact', {
-    });
+    res.render('contact', {});
 })
 
-/*Preguntar para que es esto*/
-app.get('/producto/:id', (req, res) => {
-    db.collection('libros').find({ modelo: req.params.id }).toArray((err, result) => res.send(result))
-})
+app.get('/checkout', (req, res) => {
+    res.render('checkout');
+});
+
+
+app.get('/reco/:id', (req, res) => {
+    db.collection('libros').find({
+        modelo: req.params.id
+    }).toArray((err, result) => res.send(result))
+});
+
+
+app.get('/recoPorIds', (req, res) => {
+    console.log(req.query.ids);
+    var arreglo = req.query.ids.split(',');
+    arreglo = arreglo.map(function (id) {
+        return new ObjectID(id);
+    });
+    var prod = db.collection('libros')
+        .find({
+            _id: {
+                $in: arreglo
+            }
+        })
+        .toArray((err, result) => {
+            res.send(result);
+        });
+});
